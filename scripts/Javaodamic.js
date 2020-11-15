04833ac05f3dd702f1211a2303ceec5c6ff5e7ce
joao-lucas-paes <65510376+joao-lucas-paes@users.noreply.github.com>
2020-11-15 00:23:05 -0400

    Update Javaodamic.js

--- scripts/Javaodamic.js
+++ scripts/Javaodamic.js
@@ -1,20 +1,20 @@
 function newPopup(){
     let sobrenos = window.open  (
-    '../Html/popup.html',
+    '/projeto-desenvolvimento-web-1/Html/popup.html',
     'pagina',
     "width=350, height=255, top=300, left=110, scrollbars=no, titlebar='sobre nós' " );
     sobrenos.document.writer("Quem vos escreve sou eu João Lucas Paes, quer entrar em contato comigo? Me mande um email no seguinte endereço eletronico: joao.paes@estudante.ifms.edu e se quiser entrar em contato com meu parcero de grupo Henrique? use o seguinte endereço eletronico: henrique.oliveira3@estudante.ifms.edu.br<br/>Somos estudantes do ensino médio regular do IFMS campus Campo Grande.");
 };
 function newPopup2(){
     var corte = window.open  (
-    '../Html/corte.html',
+    '/projeto-desenvolvimento-web-1/Html/corte.html',
     'pagina',
     "width=350, height=255, top=300, left=110, scrollbars=no, titlebar='sobre nós' " );
      corte.document.writer("Quem vos escreve sou eu João Lucas Paes, quer entrar em contato comigo? Me mande um email no seguinte endereço eletronico: joao.paes@estudante.ifms.edu e se quiser entrar em contato com meu parcero de grupo Henrique? use o seguinte endereço eletronico: henrique.oliveira3@estudante.ifms.edu.br<br/>Somos estudantes do ensino médio regular do IFMS campus Campo Grande.");
 };
 function newPopup3(){
     let sobrenos = window.open  (
-    '../Html/popup.html',
+    '/projeto-desenvolvimento-web-1/Html/popup.html',
     'pagina',
     "width=350, height=255, top=300, left=110, scrollbars=no, titlebar='sobre nós' " );
     sobrenos.document.writer("Quem vos escreve sou eu João Lucas Paes, quer entrar em contato comigo? Me mande um email no seguinte endereço eletronico: joao.paes@estudante.ifms.edu e se quiser entrar em contato com meu parcero de grupo Henrique? use o seguinte endereço eletronico: henrique.oliveira3@estudante.ifms.edu.br<br/>Somos estudantes do ensino médio regular do IFMS campus Campo Grande.");
